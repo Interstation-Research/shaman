bd07bc40c86ec8b62ecf20f3e81da829b422c99e
Terence Pae <terencepae@gmail.com>
2025-02-04 19:54:41 -0800

    added privy support

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
 
 import './globals.css';
+import Providers from '@/components/providers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <Providers>{children}</Providers>
         <Toaster />
       </body>
     </html>

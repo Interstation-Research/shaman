431ed921da01cc32f0c152b4b892be952bb913cb
Terence Pae <terencepae@gmail.com>
2025-02-08 15:04:35 -0800

    added add and withdraw balance dialogs

---
+++ apps/client/components/dialog-add-balance.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { useState } from 'react';
+import { DialogProps } from '@radix-ui/react-dialog';
+import { useParams } from 'next/navigation';
+import { Hex } from 'viem';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { toast } from '@/hooks/use-toast';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { useMUD } from '@/contexts/mud-context';
+
+export function DialogAddBalance(props: DialogProps) {
+  const mud = useMUD();
+  const [loading, setLoading] = useState(false);
+  const [quantity, setQuantity] = useState(10);
+  const { shamanId } = useParams();
+
+  const handleAddBalance = async () => {
+    setLoading(true);
+    try {
+      await mud?.calls.embedded?.systemCalls?.addBalance(
+        shamanId as Hex,
+        BigInt(quantity)
+      );
+
+      toast({
+        title: 'Success',
+        description: 'Balance added successfully.',
+      });
+
+      props.onOpenChange?.(false);
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: 'Error',
+        description: 'Something went wrong. Please try again.',
+        variant: 'destructive',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Dialog {...props}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Balance</DialogTitle>
+          <DialogDescription>
+            Add ZUG tokens to your Shaman Balance.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-2 py-2">
+          <div className="grid grid-cols-2 items-center gap-4">
+            <Label htmlFor="quantity" className="text-right">
+              Quantity
+            </Label>
+            <Input
+              id="quantity"
+              value={quantity}
+              max={1000000000}
+              onChange={(e) => setQuantity(Number(e.target.value))}
+              type="number"
+              className="text-right"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button disabled={loading} onClick={handleAddBalance} type="submit">
+            {loading ? 'Confirming...' : 'Add Balance'}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- apps/client/contexts/dialog-context.tsx
+++ apps/client/contexts/dialog-context.tsx
@@ -3,15 +3,25 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { DialogBuyZug } from '@/components/dialog-buy-zug';
+import { DialogAddBalance } from '@/components/dialog-add-balance';
+import { DialogWithdrawBalance } from '@/components/dialog-withdraw-balance';
 
 export type Dialogs = {
   buyZug: boolean;
+  addBalance: boolean;
+  withdrawBalance: boolean;
   openBuyZug: (open: boolean, redirect?: string) => void;
+  openAddBalance: (open: boolean, redirect?: string) => void;
+  openWithdrawBalance: (open: boolean, redirect?: string) => void;
 };
 
 const DialogContext = createContext<Dialogs>({
   buyZug: false,
+  addBalance: false,
+  withdrawBalance: false,
   openBuyZug: () => {},
+  openAddBalance: () => {},
+  openWithdrawBalance: () => {},
 });
 
 type Props = {
@@ -20,30 +30,59 @@ type Props = {
 
 export const DialogContextProvider = ({ children }: Props) => {
   const [buyZug, setBuyZug] = useState(false);
+  const [addBalance, setAddBalance] = useState(false);
+  const [withdrawBalance, setWithdrawBalance] = useState(false);
   const [redirect, setRedirect] = useState<string | undefined>(undefined);
   const router = useRouter();
+
+  const handleDialogClose = (open: boolean) => {
+    if (!open && redirect) {
+      router.push(redirect);
+      setRedirect(undefined);
+    }
+  };
 
   const openBuyZug = (open: boolean, redirectOnClose?: string) => {
     if (open && redirectOnClose) {
       setRedirect(redirectOnClose);
     }
-
-    if (!open && redirect) {
-      router.push(redirect);
-      setRedirect(undefined);
-    }
-
+    handleDialogClose(open);
     setBuyZug(open);
+  };
+
+  const openAddBalance = (open: boolean, redirectOnClose?: string) => {
+    if (open && redirectOnClose) {
+      setRedirect(redirectOnClose);
+    }
+    handleDialogClose(open);
+    setAddBalance(open);
+  };
+
+  const openWithdrawBalance = (open: boolean, redirectOnClose?: string) => {
+    if (open && redirectOnClose) {
+      setRedirect(redirectOnClose);
+    }
+    handleDialogClose(open);
+    setWithdrawBalance(open);
   };
 
   return (
     <DialogContext.Provider
       value={{
         buyZug,
+        addBalance,
+        withdrawBalance,
         openBuyZug,
+        openAddBalance,
+        openWithdrawBalance,
       }}>
       {children}
       <DialogBuyZug open={buyZug} onOpenChange={openBuyZug} />
+      <DialogAddBalance open={addBalance} onOpenChange={openAddBalance} />
+      <DialogWithdrawBalance
+        open={withdrawBalance}
+        onOpenChange={openWithdrawBalance}
+      />
     </DialogContext.Provider>
   );
 };
